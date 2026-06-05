1fc919400f441109ae45b322a1a8310ea94d3f52
cosums <114266944+cosums@users.noreply.github.com>
2026-06-05 13:44:32 -0500

    Basic room layout

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float m_moveSpeed, m_gravity;
+    [SerializeField] private CharacterController m_characterController;
+
+    private Vector2 _input;
+
+    private Vector3 _velocity;
+    private bool _grounded;
+
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        _input.x = Input.GetAxis("Horizontal");
+        _input.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _grounded = m_characterController.isGrounded;
+
+        if (_grounded && _velocity.y < -2f) _velocity.y = -2f;
+
+        Vector3 move = new Vector3(_input.x, 0, _input.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        _velocity.y -= m_gravity * Time.fixedDeltaTime;
+
+        Vector3 finalMovement = m_moveSpeed * move + Vector3.up * _velocity.y;
+        m_characterController.Move(finalMovement * Time.fixedDeltaTime);
     }
 }
