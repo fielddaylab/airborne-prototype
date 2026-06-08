db50d6301679415e405a4465cdd52990603a4eab
cosums <114266944+cosums@users.noreply.github.com>
2026-06-08 10:08:27 -0500

    better room management

--- Assets/Scripts/Inference/InferenceDoor.cs
+++ Assets/Scripts/Inference/InferenceDoor.cs
@@ -10,68 +10,68 @@ public enum DoorType
 
 public class InferenceDoor : MonoBehaviour
 {
-    [SerializeField] private InferenceRoom m_Room1, m_Room2;
-    [SerializeField] private DoorType m_DoorType;
-    [SerializeField] private BoxCollider m_BoxCollider;
+    // [SerializeField] private InferenceRoom m_Room1, m_Room2;
+    // [SerializeField] private DoorType m_DoorType;
+    // [SerializeField] private BoxCollider m_BoxCollider;
 
-    private float _horizontalDistance;
-    private bool _inContact = false;
+    // private float _horizontalDistance;
+    // private bool _inContact = false;
 
-    private void Start()
-    {
-        _horizontalDistance = m_BoxCollider.bounds.extents.x * 2;
-    }
+    // private void Start()
+    // {
+    //     _horizontalDistance = m_BoxCollider.bounds.extents.x * 2;
+    // }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            if (_inContact) return;
-            _inContact = true;
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         if (_inContact) return;
+    //         _inContact = true;
             
-            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+    //         PlayerController player = other.gameObject.GetComponent<PlayerController>();
 
-            player.currentRoom = (player.currentRoom == m_Room1) ? m_Room2 : m_Room1;
-            player.playerCamera.UpdateRoom(player.currentRoom);
+    //         player.currentRoom = (player.currentRoom == m_Room1) ? m_Room2 : m_Room1;
+    //         player.playerCamera.UpdateRoom(player.currentRoom);
 
-            switch (m_DoorType)
-            {
-                case DoorType.Horizontal:
-                    if (other.gameObject.transform.position.x > transform.position.x + _horizontalDistance / 2)
-                    {
-                        //player.PlayerCharacterController.enabled = false;
-                        other.gameObject.transform.position -= new Vector3(_horizontalDistance, 0, 0); 
-                       // player.PlayerCharacterController.enabled = true;
-                    } else
-                    {
-                        //player.PlayerCharacterController.enabled = false;
-                        other.gameObject.transform.position += new Vector3(_horizontalDistance, 0, 0); 
-                        //player.PlayerCharacterController.enabled = true;
-                    }
-                    break;
-                case DoorType.Vertical:
-                    if (other.gameObject.transform.position.y > 0)
-                    {
-                       // player.PlayerCharacterController.enabled = false;
-                        other.gameObject.transform.position -= new Vector3(0, 8, 0);
-                        //player.PlayerCharacterController.enabled = true;
-                    }
-                    else
-                    {
-                        //player.PlayerCharacterController.enabled = false;
-                        other.gameObject.transform.position += new Vector3(0, -8, 0);
-                        //player.PlayerCharacterController.enabled = true;
-                    }
-                    break;
-            }
-        }
-    }
+    //         switch (m_DoorType)
+    //         {
+    //             case DoorType.Horizontal:
+    //                 if (other.gameObject.transform.position.x > transform.position.x + _horizontalDistance / 2)
+    //                 {
+    //                     //player.PlayerCharacterController.enabled = false;
+    //                     other.gameObject.transform.position -= new Vector3(_horizontalDistance, 0, 0); 
+    //                    // player.PlayerCharacterController.enabled = true;
+    //                 } else
+    //                 {
+    //                     //player.PlayerCharacterController.enabled = false;
+    //                     other.gameObject.transform.position += new Vector3(_horizontalDistance, 0, 0); 
+    //                     //player.PlayerCharacterController.enabled = true;
+    //                 }
+    //                 break;
+    //             case DoorType.Vertical:
+    //                 if (other.gameObject.transform.position.y > 0)
+    //                 {
+    //                    // player.PlayerCharacterController.enabled = false;
+    //                     other.gameObject.transform.position -= new Vector3(0, 8, 0);
+    //                     //player.PlayerCharacterController.enabled = true;
+    //                 }
+    //                 else
+    //                 {
+    //                     //player.PlayerCharacterController.enabled = false;
+    //                     other.gameObject.transform.position += new Vector3(0, -8, 0);
+    //                     //player.PlayerCharacterController.enabled = true;
+    //                 }
+    //                 break;
+    //         }
+    //     }
+    // }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            _inContact = false;
-        }
-    }
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         _inContact = false;
+    //     }
+    // }
 }
--- Assets/Scripts/Inference/InferenceRoom.cs
+++ Assets/Scripts/Inference/InferenceRoom.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class InferenceRoom : MonoBehaviour
 {
-    [SerializeField] private InferenceDoor[] m_InferenceDoors;
-
+    [SerializeField] private BoxCollider m_BoxCollider;
     public bool PlayerOccupied = false;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerOccupied = true;
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
 
+            player.currentRoom = this;
+            player.playerCamera.UpdateRoom(player.currentRoom);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerOccupied = false;
+        }
+    }
 }
